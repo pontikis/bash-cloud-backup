cae43d28d7d09352b42bc6ba636bede4d6e6c11e
Christos Pontikis <pontikis@gmail.com>
2016-09-07 22:50:45 +0300

    minor changes

--- custom.default.sh
+++ custom.default.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-scriptpath=`dirname "$0"`
-if [ $scriptpath = "." ]; then scriptpath=''; else scriptpath=${scriptpath}/; fi
-
-# include initialize script
-source ${scriptpath}conf/initialize.sh
-
 # YOUR COMMANDS HERE
 
