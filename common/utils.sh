e241c5fbd818b062433d00af69f90fd8ca4ea571
Christos Pontikis <pontikis@gmail.com>
2016-09-07 23:56:48 +0300

    Reorganize initialize.sh (main configuration file) #26

---
+++ common/utils.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# create backup directories in case they do not exist
+if [ ! -d "$backuproot" ]; then $MKDIR -p $backuproot; fi
+if [ ! -d "$backuproot/$dir_www" ]; then $MKDIR $backuproot/$dir_www; fi
+if [ ! -d "$backuproot/$dir_mysql" ]; then $MKDIR $backuproot/$dir_mysql; fi
+if [ ! -d "$backuproot/$dir_conf" ]; then $MKDIR $backuproot/$dir_conf; fi
+if [ ! -d "$backuproot/$dir_scripts" ]; then $MKDIR $backuproot/$dir_scripts; fi
+if [ ! -d "$backuproot/$dir_docs" ]; then $MKDIR $backuproot/$dir_docs; fi
+
+# define log file
+logfile="$logfilepath/$logfilename"
+
+# create log directory in case it does not exist
+if [ ! -d "$logfilepath" ]; then $MKDIR -p $logfilepath; fi
+
+# UDF --------------------------------------------------------------------------
+function createlog {
+      dt=`$DATE "+%Y-%m-%d %H:%M:%S"`
+      logline="$dt | $1"
+      echo -e $logline; echo -e $logline >> $logfile
+}
+
+
+function rotate_delete {
+
+    dir_to_find=$1;
+    files_per_backup=$2;
+
+    if [ $days_rotation -le 0 ]; then
+        createlog "---rotating delete IS DISABLED..."
+    else
+
+        total_backups=`$FIND $dir_to_find -maxdepth 1 -type f | $WC -l`
+        total_backups=$(( $total_backups/$files_per_backup ))
+        if [ $total_backups -lt $days_rotation ]; then
+            createlog "---not enough backups ($total_backups) - no time for rotating delete..."
+        else
+            backups_to_keep=`$FIND $dir_to_find -maxdepth 1 -type f  -mtime -$days_rotation  | $WC -l`
+            backups_to_keep=$(( $backups_to_keep/$files_per_backup ))
+
+            if [ $backups_to_keep -ge $backups_to_keep_at_least ]; then
+                createlog "---rotating delete..."
+                $FIND $currentdir -mtime +$days_rotation -exec $RM {} -f \;
+            else
+                createlog "---not enough recent backups ($backups_to_keep) - rotating delete IS ABORTED..."
+            fi
+        fi
+    fi
+}
--- conf.default/initialize.sh
+++ conf.default/initialize.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 #-------------------------------------------------------------------------------
-# SCRIPT.........: initialize
-# ACTION.........: Initialize variables for backup scripts
+# SCRIPT.........: initialize.sh
+# ACTION.........: bash-cloud-backup configuration file
 # COPYRIGHT......: Christos Pontikis - http://www.pontikis.gr
 # LICENSE........: MIT (see https://opensource.org/licenses/MIT)
 # DOCUMENTATION..: See README for instructions
 #-------------------------------------------------------------------------------
 
-# PARAMETERS -------------------------------------------------------------------
+# ##############################################################################
+# CONFIGURE the following parameters
+# NOTE: do not configure parameters for a section you will not use
+# ##############################################################################
+
+# ------------------------------------------------------------------------------
+# Linux commands paths (probably you do not want to change them) ---------------
 FIND="$(which find)"
 TAR="$(which tar)"
 GZIP="$(which gzip)"
@@ -18,20 +24,78 @@ RM="$(which rm)"
 TEE="$(which tee)"
 WC="$(which wc)"
 
+# ------------------------------------------------------------------------------
+# where to backup --------------------------------------------------------------
+backuproot='/root/backup'
+
+# ------------------------------------------------------------------------------
+# web server (sites to backup) -------------------------------------------------
+# path relative to backuproot
+dir_www='www'
+# web server document root
+wwwroot='/var/www/'
+
+# ------------------------------------------------------------------------------
+# mysql (databases to backup) --------------------------------------------------
+# path relative to backuproot
+dir_mysql='mysql'
+
+mysql_user='username_here'
+mysql_password='password_here'
+
+MYSQLDUMP="$(which mysqldump)"
+
+# ------------------------------------------------------------------------------
+# configuration files (to backup) ----------------------------------------------
+# path relative to backuproot
+dir_conf='conf'
+
+# ------------------------------------------------------------------------------
+# scripts (to backup) ----------------------------------------------------------
+# path relative to backuproot
+dir_scripts='scripts'
+
+# ------------------------------------------------------------------------------
+# documents (to backup) --------------------------------------------------------
+# path relative to backuproot
+dir_docs='docs'
+
+# ------------------------------------------------------------------------------
+# logs -------------------------------------------------------------------------
+# ATTENTION do not set / at the end
+logfilepath='/root/backup/log'
+
+logfilename='backup.log'
+
+# ------------------------------------------------------------------------------
+# 7z compression and encryption (RECOMMENDED) ----------------------------------
+# if you set value other than 1, gzip compression will be used (no encryption in this case)
 use_7z=1
-# ATTENTION -------------------------------------------------------------------------------------------
+
+cmd_7z="$(which 7z) a -p$passwd_7z -mx=9 -mhe -t7z"
+filetype_7z=7z
+# you may use the following (NOT recommended) ----------------------------------
+# cmd_7z="$(which 7z) a -p$passwd_7z -mx=9 -mm=Deflate -mem=AES256 -tzip"
+# filetype_7z=zip
+
+# ATTENTION --------------------------------------------------------------------
 #Enclosing characters in double quotes preserves the literal value of all characters within the quotes,
 #with the exception of $, `, \, and, when history expansion is enabled, !.
 #...so if you escape those (and the quote itself, of course) you're probably okay.
 # http://stackoverflow.com/questions/15783701/which-characters-need-to-be-escaped-in-bash-how-do-we-know-it
 passwd_7z="YOUR_PASSWORD_HERE"
-cmd_7z="$(which 7z) a -p$passwd_7z -mx=9 -mhe -t7z"
-filetype_7z=7z
-# you should use the following (NOT recommended) -------------------------------
-# cmd_7z="$(which 7z) a -p$passwd_7z -mx=9 -mm=Deflate -mem=AES256 -tzip"
-# filetype_7z=zip
 
-MYSQLDUMP="$(which mysqldump)"
+# ------------------------------------------------------------------------------
+# rotating delete --------------------------------------------------------------
+# delete backups older than
+days_rotation=14
+# number of backups to be kept, independently of their age
+backups_to_keep_at_least=7
+
+# ------------------------------------------------------------------------------
+# Amazon S3 --------------------------------------------------------------------
+# S3 path to sync local backup - ATTENTION must end with /
+s3_plain_path='s3://bucket_name/path/to/plain_backup/'
 
 S3CMD="$(which s3cmd)"
 S3CMD_SYNC_PARAMS="--verbose --config /root/.s3cfg --delete-removed --server-side-encryption"
@@ -41,103 +105,6 @@ S3CMD_SYNC_PARAMS="--verbose --config /root/.s3cfg --delete-removed --server-sid
 # s3cmd latest version ---------------------------------------------------------
 # add server side encryption using"--server-side-encryption"
 
-days_rotation=14
-backups_to_keep_at_least=7
-backuproot='/root/backup'
-
-# ------------------------------------------------------------------------------
-# make backup directories in case they do not exist
-if [ ! -d "$backuproot" ]; then $MKDIR -p $backuproot; fi
-
-# define variables 
-logfile="$backuproot/log/backup.log"
-
-# make backup directories in case they do not exist
-if [ ! -d "$backuproot/log" ]; then $MKDIR $backuproot/log; fi
-
-# ------------------------------------------------------------------------------
-case $1 in
-www)
-    # define variables 
-    dir_www='www'
-    all_prefix_www='www'
-    wwwroot='/var/www/'
-
-    # make backup directories in case they do not exist
-    if [ ! -d "$backuproot/$dir_www" ]; then $MKDIR $backuproot/$dir_www; fi
-  ;;
-mysql)
-    # define variables 
-    dir_mysql='mysql'
-    all_prefix_mysql='mysql'
-    mysql_user='username_here'
-    mysql_password='password_here'
-
-    # make backup directories in case they do not exist
-    if [ ! -d "$backuproot/$dir_mysql" ]; then $MKDIR $backuproot/$dir_mysql; fi
-  ;;
-conf)
-    # define variables 
-    dir_conf='conf'
-
-    # make backup directories in case they do not exist
-    if [ ! -d "$backuproot/$dir_conf" ]; then $MKDIR $backuproot/$dir_conf; fi
-  ;;
-scripts)
-    # define variables 
-    dir_scripts='scripts'
-
-    # make backup directories in case they do not exist
-    if [ ! -d "$backuproot/$dir_scripts" ]; then $MKDIR $backuproot/$dir_scripts; fi
-  ;;
-docs)
-    # define variables
-    dir_docs='docs'
-
-    # make backup directories in case they do not exist
-    if [ ! -d "$backuproot/$dir_docs" ]; then $MKDIR $backuproot/$dir_docs; fi
-  ;;
-s3_plain)
-    # define variables
-    # ATTENTION must end with /
-    s3_plain_path='s3://bucket_name/path/to/plain_backup/'
-  ;;
-esac
-
-
-# UDF --------------------------------------------------------------------------
-function createlog {
-      dt=`$DATE "+%Y-%m-%d %H:%M:%S"`
-      logline="$dt | $1"
-      echo -e $logline; echo -e $logline >> $logfile
-}
-
-
-function rotate_delete {
-
-    dir_to_find=$1;
-    files_per_backup=$2;
-
-    if [ $days_rotation -le 0 ]; then
-        createlog "---rotating delete IS DISABLED..."
-    else
-
-        total_backups=`$FIND $dir_to_find -maxdepth 1 -type f | $WC -l`
-        echo "##################################total_backups $total_backups"
-        total_backups=$(( $total_backups/2 ))
-        echo "++++++++++++++++++++++++++++++++++total_backups $total_backups"
-        if [ $total_backups -lt $days_rotation ]; then
-            createlog "---not enough backups ($total_backups) - no time for rotating delete..."
-        else
-            backups_to_keep=`$FIND $dir_to_find -maxdepth 1 -type f  -mtime -$days_rotation  | $WC -l`
-            backups_to_keep=$(( $backups_to_keep/$files_per_backup ))
-
-            if [ $backups_to_keep -ge $backups_to_keep_at_least ]; then
-                createlog "---rotating delete..."
-                $FIND $currentdir -mtime +$days_rotation -exec $RM {} -f \;
-            else
-                createlog "---not enough recent backups ($backups_to_keep) - rotating delete IS ABORTED..."
-            fi
-        fi
-    fi
-}
+# ##############################################################################
+# END
+# ##############################################################################
