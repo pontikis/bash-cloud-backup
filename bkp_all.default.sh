3c3aca2ca3ab73803628b7c482236f4c4d4645c0
Christos Pontikis <pontikis@gmail.com>
2016-09-08 02:17:17 +0300

    minor changes

--- bkp_all.default.sh
+++ bkp_all.default.sh
@@ -21,36 +21,37 @@ source ${scriptpath}conf/config.sh
 # include init script
 source ${scriptpath}common/init.sh
 
-echo -e "\n$log_separator" 2>&1 | $TEE -a $logfile
+echo -e "\n$log_top_separator" 2>&1 | $TEE -a $logfile
 
-createlog "Daily filesystem backup is starting..."
+createlog "Filesystem backup is starting..."
 
 # include www backup script
+#echo -e "\n$log_separator" 2>&1 | $TEE -a $logfile
 #source ${scriptpath}bkp_www.sh
 
 # include mysql backup script
+#echo -e "\n$log_separator" 2>&1 | $TEE -a $logfile
 #source ${scriptpath}bkp_db_mysql.sh
 
 # include conf backup script
+#echo -e "\n$log_separator" 2>&1 | $TEE -a $logfile
 #source ${scriptpath}bkp_conf.sh
 
 # include scripts backup script
+#echo -e "\n$log_separator" 2>&1 | $TEE -a $logfile
 #source ${scriptpath}bkp_scripts.sh
 
 # include docs backup script
+#echo -e "\n$log_separator" 2>&1 | $TEE -a $logfile
 #source ${scriptpath}bkp_docs.sh
 
-createlog "Daily filesystem backup completed."
+createlog "Filesystem backup completed."
 
-#-------------------------------------------------------------------------------
-#createlog "####################"
+#echo -e "\n$log_separator" 2>&1 | $TEE -a $logfile
 #createlog "Daily S3 plain backup is starting..."
 #source ${scriptpath}s3-plain-sync.sh
 #createlog "Daily S3 plain backup completed."
 
-#-------------------------------------------------------------------------------
-#createlog "####################"
-#createlog "Custom commands..."
+#echo -e "\n$log_separator" 2>&1 | $TEE -a $logfile
 #source ${scriptpath}custom.sh
-#createlog "Custom backup completed."
 #-------------------------------------------------------------------------------
