89670cb20b25bae92103360dc14e7466b5665f57
Christos Pontikis <pontikis@gmail.com>
2016-09-10 14:15:03 +0300

    Consolidation of all scripts to one script: bash-cloud-backup.sh - close #30

--- bash-cloud-backup.sh
+++ bash-cloud-backup.sh
@@ -122,11 +122,11 @@ function zip_file {
     file_to_zip=$1;
 
     if [ $use_7z -eq 1 ]; then
-        createlog "---7zip $file_to_zip..."
+        createlog "7zip $file_to_zip..."
         $cmd_7z "$file_to_zip.$filetype_7z" $file_to_zip 2>&1 | $TEE -a $logfile
         $RM -f $file_to_zip
     else
-        createlog "---zip $file_to_zip..."
+        createlog "gzip $file_to_zip..."
         $GZIP -9 -f $file_to_zip 2>&1 | $TEE -a $logfile
     fi
 
