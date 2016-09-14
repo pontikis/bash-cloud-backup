7b0811b4d03130affe4d626a00c288eae4df0506
Christos Pontikis <pontikis@gmail.com>
2016-09-14 17:47:23 +0300

    Log filesize of backup file #45

--- bash-cloud-backup.sh
+++ bash-cloud-backup.sh
@@ -135,7 +135,6 @@ function createlog {
 }
 
 function log_filesize {
-
     echo -e "\nFilesize: "  2>&1 | $TEE -a $logfile_tmp
     echo -e  $(du -h "$1" | awk '{print $1}')  2>&1 | $TEE -a $logfile_tmp
     echo -e "\n"  2>&1 | $TEE -a $logfile_tmp
